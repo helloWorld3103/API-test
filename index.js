925866bc2cf41ecc7d3521c99ed4f4636d219a07
helloWorld3103 <santiago.arias4@gmail.comm>
2023-01-17 17:28:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+
+app.listen(
+    PORT,
+    () => console.log(`was created on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large'
+    })
+})
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params
+    const { logo } = req.body
+
+    if (!logo) {
+
+        res.status(418).send({ message: 'we need a logo' })
+
+    }
+
+    res.send({
+        tshirt: `👕 with you ${logo} and Id of ${id}`,
+    })
+})
