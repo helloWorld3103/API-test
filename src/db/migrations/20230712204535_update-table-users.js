dca6232104eed50ae5a08283575b758df491cf27
Santiago Arias <santiago.arias41@gmail.com>
2023-07-12 16:21:12 -0500

    update on table 'users', fixes #6

---
+++ src/db/migrations/20230712204535_update-table-users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('users', function(table) {
+        table.string('salt', 250).nullable().after('password');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('user', function(table) {
+        table.dropColumn('salt');
+      });
+};
